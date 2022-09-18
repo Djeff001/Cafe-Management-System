3d8481538141f50028d9fae97c2f9f1680ec7938
Djeff001 <nait.sider.djaffer@gmail.com>
2022-09-19 01:51:42 +0200

    create frontend project with home page

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    //BrowserAnimationsModule,
+    //FormsModule,
+    //ReactiveFormsModule,
+    //MaterialModule,
+    //FlexLayoutModule,
+    //ShareModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
